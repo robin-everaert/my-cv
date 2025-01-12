6619f9db245afdb60c29042fc2699f383a348aef
Everaert Robin <r-everaert@outlook.fr>
2025-01-12 15:09:49 +0100

    layout and settings done

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar-container d-flex justify-content-center pt-3 pb-3">
+      <router-link
+        to="/"
+        class="header-link nav-link"
+        :class="{ active: isActive('/') }"
+      >
+        {{ $t('header.nav.home') }}
+      </router-link>
+      <span> | </span>
+      <router-link
+        to="/solar"
+        class="header-link nav-link"
+        :class="{ active: isActive('/solar') }"
+      >
+        {{ $t('header.nav.about') }}
+      </router-link>
+    </nav>
+  </template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const isActive = (path) => route.path === path;
+</script>
+
+  
