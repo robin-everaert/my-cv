849fa9ea20186bb403916fdcc284c3c4de9940e3
Everaert Robin <r-everaert@outlook.fr>
2025-01-13 12:31:27 +0100

    Store developed

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import i18n from './translations/i18n';
+import { createPinia } from 'pinia';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './assets/styles/index.scss';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(i18n);
 app.use(router); 
+app.use(pinia);
 app.mount('#app'); 
